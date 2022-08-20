6263222bed1236b4f93ab797c1c99c5f8c515c07
WiFo26 <lou81804@gmail.com>
2022-08-20 17:56:11 -0500

    change smart contract address

--- hardhat-tutorial/constants/index.ts
+++ hardhat-tutorial/constants/index.ts
@@ -1,2 +1,2 @@
 export const WHITELIST_CONTRACT_ADDRESS = "0x419bb16f3C4875631A77EA1B0c9C4fe9520da49B"
-export const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/"
+export const METADATA_URL = "https://crypto-devs-nft-lake.vercel.app/api/"
--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,4 +1,4 @@
-export const NFT_CONTRACT_ADDRESS = "0x2138DdB08089B1E278161F5cEddbd469c4B68a4a"
+export const NFT_CONTRACT_ADDRESS = "0x99D8B663E71Ba10ccB807Ad990B9c60Ce0538b40"
 export const abi = [
   {
     "inputs": [
